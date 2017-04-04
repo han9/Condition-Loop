0e7578a83ff3178eda3a347e3ba94a6269346392
hankyu.C <hankyu859@gmail.com>
2017-04-04 21:07:11 +0900

    조건과 반복의 자바 소스 추가
    조건과 반복의 자바 소스 추가

---
+++ PriorityOP.java
@@ -0,0 +1,15 @@
+package ch6;
+
+public class PriorityOP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(3+4>>2>5); //false
+		System.out.println(((3+4)>>2)>5); //false
+		System.out.println(3*4&4-3<<5); // 0
+		System.out.println(3*4&(4-3<<5)); //0
+		System.out.println((3*4&4-3)<5); //true
+		System.out.println((1&0) !=1&&3<<2>5); //true
+	}
+
+}
